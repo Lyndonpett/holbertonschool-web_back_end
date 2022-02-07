1a481c4a442de6aa9a7f9a7bc354502dbe2c54b4
lyndonpett <lyndonpett98@gmail.com>
2022-02-07 10:57:44 -0600

    eslint

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -4,16 +4,16 @@ import uploadPhoto from './5-photo-reject';
 export default async function handleProfileSignup(
   firstName,
   lastName,
-  fileName
+  fileName,
 ) {
   const user = await signUpUser(firstName, lastName).then((data) => ({
     status: 'fulfilled',
-    value: data
+    value: data,
   }));
 
   const file = await uploadPhoto(fileName).catch((error) => ({
     status: 'rejected',
-    value: error.toString()
+    value: error.toString(),
   }));
 
   return Promise.resolve([user, file]);
