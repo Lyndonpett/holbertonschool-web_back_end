a4853e7047f0894fa78dbe13d26ba98e4a789ac4
lyndonpett <lyndonpett98@gmail.com>
2022-02-07 10:52:20 -0600

    test

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,20 +1,16 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default async function handleProfileSignup(
-  firstName,
-  lastName,
-  fileName,
-) {
-  const user = await signUpUser(firstName, lastName).then((data) => ({
-    status: 'fulfulled',
+export default async function handleProfileSignup(firstName, lastName, fileName) {
+  const userData = await signUpUser(firstName, lastName).then((data) => ({
+    status: 'fulfilled',
     value: data,
   }));
 
-  const file = await uploadPhoto(fileName).catch((error) => ({
+  const fileData = await uploadPhoto(fileName).catch((err) => ({
     status: 'rejected',
-    value: error.toString(),
+    value: err.toString(),
   }));
 
-  return Promise.resolve([user, file]);
+  return Promise.resolve([userData, fileData]);
 }
