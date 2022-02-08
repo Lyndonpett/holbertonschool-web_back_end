9f90a3ece4bfc32f74f2221483c006e0bda1c5ad
lyndonpett <lyndonpett98@gmail.com>
2022-02-08 12:52:27 -0600

    Updated if checks

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,15 +1,8 @@
 export default class HolbertonCourse {
   constructor(name, length, students) {
-    if (typeof name !== 'string') throw new TypeError('Name must be a string');
-    if (typeof length !== 'number') throw new TypeError('Length must be a number');
-    if (!Array.isArray(students)) throw Error('Students must be an array');
-    for (const arr of students) {
-      if (typeof arr !== 'string') throw Error('Student must be a string');
-
-      this._name = name;
-      this._length = length;
-      this._students = students;
-    }
+    this.name = name;
+    this.length = length;
+    this.students = students;
   }
 
   get name() {
@@ -41,11 +34,13 @@ export default class HolbertonCourse {
   }
 
   set students(value) {
-    if (!Array.isArray(this.students)) throw new TypeError('Students must be an array');
-
-    for (const arr of value) {
-      if (typeof arr !== 'string') throw new TypeError('Student must be a string');
+    if (
+      typeof value === 'object'
+      && value.every((i) => typeof i === 'string')
+    ) {
       this._students = value;
+    } else {
+      throw new TypeError('Students must be an array of numbers');
     }
   }
 }
