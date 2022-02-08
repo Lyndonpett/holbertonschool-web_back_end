796cabfc1d808e5a850bd09e059fd6a97ae2ab07
lyndonpett <lyndonpett98@gmail.com>
2022-02-08 10:22:50 -0600

    Adding task 2

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,49 @@
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    if (typeof name !== 'string') throw new TypeError('Name must be a string');
+    if (typeof length !== 'number') throw new TypeError('Length must be a number');
+    if (Array.isArray(students) === false) throw new TypeError('Students must be an array');
+
+    this._name = name;
+    this._length = length;
+    this._students = students;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(value) {
+    if (typeof value === 'string') {
+      this._name = value;
+    } else {
+      throw new TypeError('Name must be a string');
+    }
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  set length(value) {
+    if (typeof value === 'number') {
+      this._length = value;
+    } else {
+      throw new TypeError('Length must be a number');
+    }
+  }
+
+  get students() {
+    return this._students;
+  }
+
+  set students(value) {
+    if (
+      Array.isArray(value) === true
+      && value.every((index) => typeof index === 'string')
+    ) {
+      return this._students;
+    }
+    throw new TypeError('Students must be an array of numbers');
+  }
+}
