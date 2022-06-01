ab2b7658d6fe741f17575d682f90e0bb59a4d16f
lyndonpett <lyndonpett98@gmail.com>
2022-06-01 09:22:19 -0500

    Adding tasks 3 and 4

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,7 @@
+#!/env/usr/bin/python3
+"""Type annotations for to_str function"""
+
+
+def to_str(n: float) -> str:
+    """Convert a number to a string"""
+    return str(n)
---
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -0,0 +1,8 @@
+#!/env/usr/bin/python3
+"""Type annotations for python variables"""
+
+
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = "Holberton"
