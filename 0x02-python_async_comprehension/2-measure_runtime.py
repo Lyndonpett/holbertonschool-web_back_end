769e28b8e86cf90cded9b146b95874442279c67a
lyndonpett <lyndonpett98@gmail.com>
2022-06-06 17:25:47 -0500

    maybe?

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -10,7 +10,6 @@ async_comp = __import__('1-async_comprehension').async_comprehension
 async def measure_runtime() -> float:
     """returns the runtime of the async_comprehension function"""
     start: float = time()
-    await gather(
-        async_comp(), async_comp(), async_comp(), async_comp())
+    await gather(async_comp(), async_comp(), async_comp(), async_comp())
     end: float = time()
     return end - start
