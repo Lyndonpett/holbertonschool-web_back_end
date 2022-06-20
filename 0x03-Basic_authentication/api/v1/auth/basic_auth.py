62f5f73b795039bafbc5fa669dd0363b0efa88fb
lyndonpett <lyndonpett98@gmail.com>
2022-06-20 08:42:55 -0600

    Adding task 10

--- 0x03-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x03-Basic_authentication/api/v1/auth/basic_auth.py
@@ -4,7 +4,8 @@
 
 from api.v1.auth.auth import Auth
 import base64
-from typing import Tuple
+from typing import Tuple, TypeVar
+from models.user import User
 
 
 class BasicAuth(Auth):
@@ -35,3 +36,21 @@ class BasicAuth(Auth):
                     return decoded_base64_authorization_header.split(':')
 
         return None, None
+
+    def user_object_from_credentials(self, user_email: str, user_pwd: str) -> TypeVar('User'):
+        """Return user based on email and password"""
+        if user_email is None or type(user_email) is not str:
+            return None
+
+        if user_pwd is None or type(user_pwd) is not str:
+            return None
+
+        try:
+            searchList = User.search({'email': user_email})
+
+            for user in searchList:
+                if user.is_valid_password(user_pwd):
+                    return user
+
+        except Exception:
+            return None
