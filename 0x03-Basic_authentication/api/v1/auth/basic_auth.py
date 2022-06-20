03d6fc75bf62b1088ac392aae0ddf0cbf3b61287
lyndonpett <lyndonpett98@gmail.com>
2022-06-20 08:32:36 -0600

    Adding task 8

--- 0x03-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x03-Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,6 +3,7 @@
 
 
 from api.v1.auth.auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
@@ -16,3 +17,11 @@ class BasicAuth(Auth):
                     return authorization_header[6:]
 
         return None
+
+    def decode_base64_authorization_header(self, base64_authorization_header: str) -> str:
+        """Decodes base64 header"""
+        try:
+            return base64.b64decode(base64_authorization_header).decode('utf-8')
+
+        except Exception:
+            return None
