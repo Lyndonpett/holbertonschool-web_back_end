c1e655a859c644050bf291be2f54442997c16228
lyndonpett <lyndonpett98@gmail.com>
2022-06-27 09:55:03 -0500

    Adding task 5

--- 0x05-user_authentication_service/auth.py
+++ 0x05-user_authentication_service/auth.py
@@ -3,7 +3,28 @@
 
 
 from bcrypt import checkpw, hashpw, gensalt
+from sqlalchemy.orm.exc import NoResultFound
 from db import DB
+from user import User
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """Register a new user"""
+        try:
+            self._db.find_user_by(email=email)
+            raise ValueError("User {} already exists".format(email))
+
+        except NoResultFound:
+            passwrd = _hash_password(password)
+            user = self._db.add_user(email, passwrd)
+            return user
 
 
 def _hash_password(password: str) -> bytes:
