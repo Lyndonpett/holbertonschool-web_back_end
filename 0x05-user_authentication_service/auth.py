a1aa2851e838841817b78470c490eb62678bd3e2
lyndonpett <lyndonpett98@gmail.com>
2022-06-27 10:32:17 -0500

    Adding task 12

--- 0x05-user_authentication_service/auth.py
+++ 0x05-user_authentication_service/auth.py
@@ -46,6 +46,14 @@ class Auth:
         except Exception:
             return None
 
+    def get_user_from_session_id(self, session_id: str) -> User:
+        """Get a user from a session ID"""
+        try:
+            user = self._db.find_user_by(session_id=session_id)
+            return user
+        except Exception:
+            return None
+
 def _hash_password(password: str) -> bytes:
     """Hash a password"""
     return hashpw(password.encode(), gensalt())
