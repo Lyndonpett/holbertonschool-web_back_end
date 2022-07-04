9a4ce96de12a0734abff3cba54c099c7811ce341
lyndonpett <lyndonpett98@gmail.com>
2022-07-04 10:24:26 -0500

    Adding task 5

--- 0x00-Unittests_and_integration_tests/test_client.py
+++ 0x00-Unittests_and_integration_tests/test_client.py
@@ -5,7 +5,7 @@
 from client import GithubOrgClient
 from parameterized import parameterized
 from unittest import TestCase, mock
-from unittest.mock import patch, MagicMock
+from unittest.mock import patch, MagicMock, PropertyMock
 
 
 class TestGithubOrgClient(TestCase):
@@ -21,3 +21,11 @@ class TestGithubOrgClient(TestCase):
         "test org method"
         client = GithubOrgClient(org_name)
         self.assertEqual(client.org, {'key': 'value'})
+
+    def test_public_repos_url(self):
+        """Test public_repos_url method"""
+        with patch("client.get_json",
+                   new_callable=PropertyMock,
+                   return_value={"repos_url": "url"}):
+            client = GithubOrgClient("google")
+            self.assertEqual(client._public_repos_url, "url")
