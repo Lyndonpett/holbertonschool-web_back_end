643873314fcf53c3f289bbedac0ef0a4f8c1c208
lyndonpett <lyndonpett98@gmail.com>
2022-07-04 10:36:03 -0500

    Adding task 7

--- 0x00-Unittests_and_integration_tests/test_client.py
+++ 0x00-Unittests_and_integration_tests/test_client.py
@@ -41,3 +41,12 @@ class TestGithubOrgClient(TestCase):
             self.assertEqual(cls.public_repos, 'url')
             license.assert_called_once()
             mock_repo.assert_called_once()
+
+    @parameterized.expand([
+        ({"license": {"key": "my_license"}}, "my_license", True),
+        ({"license": {"key": "other_license"}}, "my_license", False),
+    ])
+    def test_has_license(self, repo, license_key, expected):
+        """Test has_license method"""
+        cls = GithubOrgClient('org_name')
+        self.assertEqual(cls.has_license(repo, license_key), expected)
