1477f99c0fc0c5a98e0ba533c791f2ee082251bf
lyndonpett <lyndonpett98@gmail.com>
2022-07-08 12:49:21 -0500

    Adding task 1

---
+++ 0x01-i18n/1-app.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+"""Basic Flask App"""
+
+
+from flask import Flask, render_template
+from flask_babel import Babel
+
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config(object):
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app.config.from_object(Config)
+
+
+@app.route('/', methods=['GET'])
+def index():
+    """Index page"""
+    return render_template('1-index.html')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True)
