ba4c77abb18b74f3359480c8095d8112c41a6483
lyndonpett <lyndonpett98@gmail.com>
2022-07-14 14:36:21 -0500

    Adding task 0

---
+++ 0x01-MySQL_Advanced/0-uniq_users.sql
@@ -0,0 +1,7 @@
+-- script to create table users with id, email, name
+CREATE TABLE IF NOT EXISTS users (
+  id INT NOT NULL AUTO_INCREMENT,
+  email VARCHAR(255) NOT NULL UNIQUE,
+  name VARCHAR(255),
+  PRIMARY KEY (id)
+);
