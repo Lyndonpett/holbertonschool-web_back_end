a47735f0f6efe8a7e12b53d12ada0686dbd63715
lyndonpett <lyndonpett98@gmail.com>
2022-07-15 13:12:35 -0500

    Adding task 7

---
+++ 0x01-MySQL_Advanced/7-average_score.sql
@@ -0,0 +1,16 @@
+-- creates a stored procedure ComputeAverageScoreForUser that computes and
+-- store the average score for a student. Note: An average score can be a decimal
+DELIMITER //
+
+CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
+BEGIN
+	SET @average_score := (
+		SELECT AVG(score)
+		FROM corrections
+		WHERE corrections.user_id = user_id);
+	UPDATE users
+	SET average_score = @average_score
+	WHERE id = user_id;
+END //
+
+DELIMITER ;
