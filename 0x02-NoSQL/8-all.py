660b1fae6535c55ff2f7238aefa606195abe790a
lyndonpett <lyndonpett98@gmail.com>
2022-07-19 10:39:35 -0500

    Adding task 8

---
+++ 0x02-NoSQL/8-all.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""Using mongodb in python, helper functions"""
+
+
+from pymongo import MongoClient
+
+
+def list_all(mongo_collection):
+    """List all documents in a collection"""
+    if mongo_collection is None:
+        return []
+
+    return mongo_collection.find()
