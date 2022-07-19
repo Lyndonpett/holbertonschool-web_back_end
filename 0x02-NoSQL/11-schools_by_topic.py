714f79bef44f3a5c0baaf3f1055452dcad85136e
lyndonpett <lyndonpett98@gmail.com>
2022-07-19 10:50:53 -0500

    Adding task 11

---
+++ 0x02-NoSQL/11-schools_by_topic.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""returns the list of school having a specific topic"""
+
+
+from pymongo import MongoClient
+
+
+def schools_by_topic(mongo_collection, topic):
+    """returns the list of school having a specific topic"""
+    if mongo_collection is None:
+        return []
+
+    return mongo_collection.find({'topics': topic})
