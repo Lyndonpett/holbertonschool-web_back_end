59d994f7d4a756d2aec7493353b7f18b502ad186
lyndonpett <lyndonpett98@gmail.com>
2022-07-20 11:11:53 -0500

    Chmod

--- 0x02-NoSQL/12-log_stats.py
+++ 0x02-NoSQL/12-log_stats.py
@@ -7,25 +7,20 @@ from pymongo import MongoClient
 
 def loggedStats():
     """provides some stats about Nginx logs stored in MongoDB"""
-    # connect to MongoDB and get logs
     client = MongoClient()
     db = client.logs
 
-    # get nginx logs
     nginxLogs = db.nginx
 
-    # print number of docs in the collection
     print(f"{nginxLogs.count_documents({})} logs")
 
     print("Methods:")
     methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
 
-    # loop each method and print docs with that method
     for method in methods:
         print(f"\tmethod {method}: " +
               f"{nginxLogs.count_documents({'method': method})}")
 
-    # print the number of documents with method=GET and path=/status
     print(f"{nginxLogs.count_documents({'method': 'GET', 'path': '/status'})} \
 status check")
 
