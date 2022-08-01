0a6331f6368ff8a38ad6369fe1268c0ede942aec
lyndonpett <lyndonpett98@gmail.com>
2022-08-01 11:17:47 -0500

    Adding task 3

---
+++ 0x01-unittests_in_js/3-payment.test.js
@@ -0,0 +1,15 @@
+const sendPaymentRequestToApi = require('./3-payment');
+const Utils = require('./utils');
+const expect = require('chai').expect;
+const sinon = require('sinon');
+
+describe('sendPaymentRequestToApi', () => {
+      it('Make sure math for sendPaymentRequestToAPI is the same as calculateNumber', () => {
+        const spiedFunction = sinon.spy(Utils, 'calculateNumber');
+
+        sendPaymentRequestToApi(100, 20);
+
+        expect(spiedFunction.calledWith('SUM', 100, 20)).to.be.true;
+        spy.restore();
+      })
+    }
---
+++ 0x01-unittests_in_js/utils.js
@@ -0,0 +1,23 @@
+// Utils module
+
+
+const Utils = {
+  calculateNumber = (type, a, b) => {
+    const whichType = type.toLowerCase();
+    if (isNaN(a) || isNaN(b)) throw new TypeError('Arguments must be numbers');
+
+    switch (whichType) {
+      case 'sum':
+        return Math.round(a) + Math.round(b);
+      case 'subtract':
+        return Math.round(a) - Math.round(b);
+      case 'divide':
+        if (Math.round(b) === 0) return 'Error';
+        return Math.round(a) / Math.round(b);
+      default:
+        throw new TypeError('Arguments must be SUM, SUBTRACT or DIVIDE');
+    }
+  },
+};
+
+module.exports = Utils;
