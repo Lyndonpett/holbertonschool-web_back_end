54b76d9c0ec9d714f886a7c8ab8f1ade372684c5
lyndonpett <lyndonpett98@gmail.com>
2022-08-01 10:41:47 -0500

    Trying task 2 again

--- 0x01-unittests_in_js/2-calcul_chai.test.js
+++ 0x01-unittests_in_js/2-calcul_chai.test.js
@@ -3,26 +3,26 @@ const expect = require('chai').expect;
 
 describe('calculateNumber', () => {
   it('taking in postive num and return the result', () => {
-    expect(calculateNumber('SUM', 1, 2)).to.equal(3);
-    expect(calculateNumber('SUBTRACT', 1.4, 4.5)).to.equal(-4);
-    expect(calculateNumber('DIVIDE', 1.5, 2)).to.equal(1);
+    expect(calculateNumber('SUM', 1, 2)).to.be.eq(3);
+    expect(calculateNumber('SUBTRACT', 1.4, 4.5)).to.be.eq(-4);
+    expect(calculateNumber('DIVIDE', 1.5, 2)).to.be.eq(1);
   });
 
   it('taking in negative nums and return the result', () => {
-    expect(calculateNumber('SUM', 1, -2)).to.equal(-1);
-    expect(calculateNumber('SUBTRACT', -1.4, 4.5)).to.equal(-6);
-    expect(calculateNumber('DIVIDE', -1.5, 2)).to.equal(-0.5);
+    expect(calculateNumber('SUM', 1, -2)).to.be.eq(-1);
+    expect(calculateNumber('SUBTRACT', -1.4, 4.5)).to.be.eq(-6);
+    expect(calculateNumber('DIVIDE', -1.5, 2)).to.be.eq(-0.5);
   });
 
   it('taking in a 0 while dividing', () => {
-    expect(calculateNumber('DIVIDE', 1, 0)).to.equal('Error');
+    expect(calculateNumber('DIVIDE', 1, 0)).to.be.eq('Error');
   });
 
   it('taking in type SUM', () => {
-    expect(calculateNumber('SUM', 1, 2)).to.equal(3);
-    expect(calculateNumber('SUM', 2, 5)).to.equal(7);
-    expect(calculateNumber('SUM', 4, -5)).to.equal(-1);
-    expect(calculateNumber('SUM', 3, 2)).to.equal(5);
-    expect(calculateNumber('SUM', -2, -5)).to.equal(-7);
+    expect(calculateNumber('SUM', 1, 2)).to.be.eq(3);
+    expect(calculateNumber('SUM', 2, 5)).to.be.eq(7);
+    expect(calculateNumber('SUM', 4, -5)).to.be.eq(-1);
+    expect(calculateNumber('SUM', 3, 2)).to.be.eq(5);
+    expect(calculateNumber('SUM', -2, -5)).to.be.eq(-7);
   });
 });
