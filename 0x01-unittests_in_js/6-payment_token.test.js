81aad987021b5d59f555cdb7da7065bd99c0c809
lyndonpett <lyndonpett98@gmail.com>
2022-08-01 12:14:36 -0500

    Adding task 6

--- 0x01-unittests_in_js/6-payment_token.test.js
+++ 0x01-unittests_in_js/6-payment_token.test.js
@@ -3,8 +3,9 @@ const expect = require('chai').expect;
 
 describe('getPaymentTokenFromAPI', () => {
   it('Tests that a new promise is returned', (done) => {
-    const result = getPaymentTokenFromAPI(true);
-    expect(result).to.be.an.instanceof(Promise);
-    done();
+    getPaymentTokenFromAPI(true).then((response) => {
+      expect(response.data).to.equal('Successful response from the API');
+      done();
+    });
   });
 });
